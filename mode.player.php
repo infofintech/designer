068767c8cfd3e6836caef35350ccb4e23609727c
Karl August Nestor <jesuscaesar@outlook.com>
2023-06-12 17:18:04 +1000

    Latest update

---
+++ mode.player.php
@@ -0,0 +1,11 @@
+<div class='back'>
+<?php
+$parent = $request['ref'];
+include 'workspace.php';
+?>
+</div>
+<div class='panel' <?php if ($module['spectate'] == 1) { ?> hidden <?php } ?>>
+<p align="center" class="block">
+<video style="width:92%;height:90%;" id="video" src="<?=$request['input'];?>" controls autoplay="yes" onended="if (moduleRepeat.value != 0) { this.pause(); this.load(); this.play(); }">
+</p>
+</div>
